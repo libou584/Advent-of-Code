68e3c027acc915823372c909d29c0dd01e9e1457
lilian <li4bou@gmail.com>
2024-12-07 09:55:56 +0100

    2024, 7

---
+++ 2024/day07/prob2.py
@@ -0,0 +1,24 @@
+import re
+from math import log10
+
+
+def ok(equation) :
+    if equation[1] > equation[0] :
+        return False
+    if len(equation) == 2 :
+        return equation[0] == equation[1]
+    add_equation = [equation[0], equation[1] + equation[2]] + equation[3:]
+    mult_equation = [equation[0], equation[1] * equation[2]] + equation[3:]
+    conc_equation = [equation[0], equation[1]*10**(int(log10(equation[2])+1)) + equation[2]] + equation[3:]
+    return ok(add_equation) or ok(mult_equation) or ok(conc_equation)
+
+
+if __name__ == "__main__" :
+    file = open("2024/day07/input.txt")
+    equations = [[int(n) for n in re.findall("\d+", line)] for line in file.readlines()]
+    file.close()
+    n = 0
+    for equation in equations :
+        if ok(equation) :
+            n += equation[0]
+    print(n)
